1ac6b5492341f543796d53b74f2e015936340c8b
Reinaldo Alves <102365824+Kingnaldo1337@users.noreply.github.com>
2025-01-15 16:46:14 -0300

    Delete TaskChest directory

--- TaskChest/src/api/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { singUpIn } = require('../controllers/login')
-
-const loginRoutes = express.Router({ mergeParams: true });
-
-loginRoutes.post('/sing-up-in', singUpIn);
-
-module.exports = loginRoutes;
